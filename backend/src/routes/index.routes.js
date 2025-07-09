d2bd3c497ae74d332c3816f6745b07dbd82f3987
Sebastian <sebamedina273@gmail.com>
2025-07-09 12:07:41 -0400

    mod: Modifique la ruta del index

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,11 +3,13 @@ import { Router } from "express";
 import authRoutes from "./auth.routes.js"
 import userRoutes from "./user.routes.js";
 import activityRoutes from "./activity.routes.js";
+import petitionRoutes from "./petition.routes.js";
 
 const router = new Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/activities", activityRoutes);
+router.use("/petitions", petitionRoutes);
 
 export default router;
