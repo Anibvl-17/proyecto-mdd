a87e173a3d4c70724a58938268a41f404d509d40
Sebastian <sebamedina273@gmail.com>
2025-07-16 14:28:47 -0400

    mod: Cambié el nombre de una ruta, agregué una nueva para obtener peticiones y mejoré la lógica de acceso por rol

--- backend/src/routes/petition.routes.js
+++ backend/src/routes/petition.routes.js
@@ -4,17 +4,28 @@ import { Router } from "express";
 import { authenticateJwt } from "../middleware/authentication.middleware.js";
 import { isAdmin } from "../middleware/authorization.middleware.js";
 import { isVecino } from "../middleware/authorization.middleware.js";
-import { getPetition, getPetitionId, createPetition, updatePetition, deletePetition, setRevised } from "../controllers/petition.controller.js";
+import { 
+    getAllPetitions, 
+    getUserPetitions, 
+    getPetitionId, 
+    createPetition, 
+    updatePetition, 
+    deletePetition, 
+    setRevised 
+} from "../controllers/petition.controller.js";
 
 const router = Router();
 
 router.use(authenticateJwt);
-router.get("/", getPetition);
-router.get("/:id", getPetitionId);
-router.post("/", isVecino, createPetition);
-router.put("/:id", isVecino, updatePetition); 
+
+router.get("/", isAdmin, getAllPetitions);
 router.put("/setRevised/:id", isAdmin, setRevised);
 
+router.get("/getUser", isVecino, getUserPetitions);
+router.post("/", isVecino, createPetition);
+router.put("/:id", isVecino, updatePetition); 
 router.delete("/:id", isVecino, deletePetition);
+
+router.get("/:id", getPetitionId);
 
 export default router;
