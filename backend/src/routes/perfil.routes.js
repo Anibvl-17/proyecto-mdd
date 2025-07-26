e1735e4506699bdcea8ba4f4d15099c4eb9db702
Claudio Candia Vargas <claudio.candia2301@alumnos.ubiobio.cl>
2025-07-26 18:19:04 -0400

    fix: estandarizar nombre de ruta

--- backend/src/routes/perfil.routes.js
+++ backend/src/routes/perfil.routes.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const router = express.Router();
+"use strict";
+
+import { Router } from "express";
+
+const router = new Router();
 
 const { getPerfil, updatePerfil } = require('../controllers/perfilController');
 const authMiddleware = require('../middlewares/auth');
 const { validarActualizacionPerfil } = require('../validations/user.validation');
+
+router.
 
 router.get('/', authMiddleware, getPerfil);
 router.put('/', authMiddleware, validarActualizacionPerfil, updatePerfil);
