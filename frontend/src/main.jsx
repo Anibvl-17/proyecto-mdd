d8760f60e1a47aec00c4254c3c7f86da0ded7c8b
Claudio Candia Vargas <claudio.candia2301@alumnos.ubiobio.cl>
2025-07-29 17:54:09 -0400

    fix: falta slash antes del nombre de ruta

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "activities",
+        path: "/activities",
         element: <Activities />
       },
       {
