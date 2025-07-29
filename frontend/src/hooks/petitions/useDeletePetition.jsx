f7b447b93afc1700c5f6fdfe696a4c3bef90c1e8
Sebastian <sebamedina273@gmail.com>
2025-07-29 17:14:23 -0400

    add: Agregue la carpeta peticiones con los hooks de crear, editar, eliminar, obtener y revisar

---
+++ frontend/src/hooks/petitions/useDeletePetition.jsx
@@ -0,0 +1,57 @@
+import { DeletePetition } from "@services/petitions.service.js";
+import Swal from "sweetalert2";
+
+async function confirmDeletePetition() {
+    const result = await Swal.fire({
+        title: "¿Estás seguro?",
+        text: "No podrás deshacer esta acción.",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonText: "Sí, eliminar",
+        cancelButtonText: "Cancelar",
+    });
+    
+    return result.isConfirmed;
+}
+
+export const useDeletePetition = (fetchPetitions) => {
+    const handleDeletePetition = async(petitionId, isReviewed = false) => {
+        if (isReviewed){
+            Swal.fire({
+                icon: "warning",
+                title: "No se puede eliminar",
+                text: "La petición ya fue revisada y no puede ser eliminada.",
+                confirmButtonText: "Aceptar",
+            });
+            return;
+        }
+
+        try{
+            const isConfirmed = await confirmDeletePetition();
+            if (isConfirmed){
+                const response = await DeletePetition(petitionId);
+                if (response){
+                    Swal.fire({
+                        title: "Petición eliminada",
+                        text: "La petición ha sido eliminada correctamente.",
+                        icon: "success",
+                        confirmButtonText: "Aceptar",
+                    });
+                    await fetchPetitions();
+                }
+            }    
+        }catch(error){
+            console.error("Error al eliminar la petición:", error);
+            Swal.fire({
+                title: "Error",
+                text: "No se pudo eliminar la petición.",
+                icon: "error",
+                confirmButtonText: "Aceptar",
+            });
+        }
+    };
+
+    return {handleDeletePetition};
+};
+
+export default useDeletePetition;
