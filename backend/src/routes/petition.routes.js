eec2f288c02db934057c90b81484686a65a2c51d
Sebastian <sebamedina273@gmail.com>
2025-07-29 16:52:59 -0400

    del: Elimine la funcion  isVecino y modifique sus rutas correspondientes

--- backend/src/routes/petition.routes.js
+++ backend/src/routes/petition.routes.js
@@ -3,7 +3,6 @@
 import { Router } from "express";
 import { authenticateJwt } from "../middleware/authentication.middleware.js";
 import { isAdmin } from "../middleware/authorization.middleware.js";
-import { isVecino } from "../middleware/authorization.middleware.js";
 import { 
     getAllPetitions, 
     getUserPetitions, 
@@ -20,9 +19,9 @@ router.use(authenticateJwt);
 router.get("/", isAdmin, getAllPetitions);
 router.put("/setRevised/:id", isAdmin, setRevised);
 
-router.get("/getUser", isVecino, getUserPetitions);
-router.post("/", isVecino, createPetition);
-router.put("/:id", isVecino, updatePetition); 
-router.delete("/:id", isVecino, deletePetition);
+router.get("/getUser", getUserPetitions);
+router.post("/", createPetition);
+router.put("/:id", updatePetition); 
+router.delete("/:id", deletePetition);
 
 export default router;
